cbde156fbea6534e8bf3c36a64d589565acf5c15
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2023-11-21 05:21:28 -0300

    Don't print price table if t is <= 0

--- public/rational.cjs
+++ public/rational.cjs
@@ -420,6 +420,7 @@ function priceTable(np, pv, t, pmt) {
   let dataTable = [
     ["Mês", "Prestação", "Juros", "Amortização", "Saldo Devedor"],
   ];
+  if (t <= 0) return;
   let pt = getDownPayment() ? pmt : 0;
   let jt = 0;
   let at = 0;
