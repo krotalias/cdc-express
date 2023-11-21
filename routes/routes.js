e667d63abce2114ecfdad820b9e60d225fc1c8db
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2023-11-21 05:01:11 -0300

    Testing pmt value before calling getInterest

--- routes/routes.js
+++ routes/routes.js
@@ -76,7 +76,7 @@ function createHTML(arr, prt = false) {
   let [np, t, pv, pp, pb, nb] = arr;
   t *= 0.01;
 
-  let pmt = 0;
+  let pmt = pp / np;
   let cf = 0;
   let i = 0;
   let ti = 0;
@@ -85,6 +85,11 @@ function createHTML(arr, prt = false) {
 
   try {
     if (t === 0) {
+      if (pmt >= pv) {
+        throw new Error(
+          `Prestação (\$${pmt.toFixed(2)}) é maior do que o empréstimo`
+        );
+      }
       [ti, i] = rational.getInterest(pp, pv, np);
       t = ti * 0.01;
     }
